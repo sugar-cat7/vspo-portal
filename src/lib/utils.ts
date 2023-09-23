fdd551ab0451971b89c5aee35f47b8f3a55c76b5
sugar-cat7 <study.king7777@gmail.com>
2023-09-24 00:25:41 +0900

    fix：MPA

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -785,3 +785,12 @@ export const convertThumbnailQualityInObjects = <T extends HasThumbnailUrl>(
     // .replace("hqdefault", "mqdefault")
   }));
 };
+
+export const isValidDate = (dateString: string) => {
+  const regEx = /^\d{4}-\d{2}-\d{2}$/;
+  if (!dateString.match(regEx)) return false; // Invalid format
+  const d = new Date(dateString);
+  const dNum = d.getTime();
+  if (!dNum && dNum !== 0) return false; // NaN value, Invalid date
+  return d.toISOString().slice(0, 10) === dateString;
+};
