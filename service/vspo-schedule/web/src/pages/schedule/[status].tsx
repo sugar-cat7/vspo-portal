850b6c9a57d045bca13793fcc4f016b1c6a890d8
sugar <69241625+sugar-cat7@users.noreply.github.com>
2024-03-26 07:53:05 +0900

    fix: switch fallback true (#282)
    * fix: switch fallback true
    
    * fix: fallback

--- service/vspo-schedule/web/src/pages/schedule/[status].tsx
+++ service/vspo-schedule/web/src/pages/schedule/[status].tsx
@@ -19,6 +19,8 @@ import { freechatVideoIds } from "@/data/freechat-video-ids";
 import { fetchEvents, fetchLivestreams } from "@/lib/api";
 import { VspoEvent } from "@/types/events";
 import Link from "next/link";
+import { useRouter } from "next/router";
+import { Loading } from "@/components/Elements";
 
 type Params = {
   status: string;
@@ -56,6 +58,11 @@ const HomePage: NextPageWithLayout<LivestreamsProps> = ({
   eventsByDate,
   dateTabsInfo,
 }) => {
+  const router = useRouter();
+  if (router.isFallback) {
+    return <Loading />;
+  }
+
   if (!dateTabsInfo) {
     return (
       <LivestreamCards
@@ -123,7 +130,7 @@ export const getStaticPaths: GetStaticPaths<Params> = () => {
     const formattedDate = formatWithTimeZone(newDate, "ja", "yyyy-MM-dd");
     datePaths.push({ params: { status: formattedDate } });
   }
-  return { paths: [...statusPaths, ...datePaths], fallback: false };
+  return { paths: [...statusPaths, ...datePaths], fallback: true };
 };
 
 export const getStaticProps: GetStaticProps<LivestreamsProps, Params> = async ({
