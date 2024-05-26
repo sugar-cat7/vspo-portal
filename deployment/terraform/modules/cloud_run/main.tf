59c9a186fcd8477986c3fc48c53b962e89faa73f
sugar-cat7 <study.king7777@gmail.com>
2024-05-26 23:22:40 +0900

    fix

--- deployment/terraform/modules/cloud_run/main.tf
+++ deployment/terraform/modules/cloud_run/main.tf
@@ -5,7 +5,7 @@ resource "google_cloud_run_v2_job" "vspo_portal_cloud_run_v2_job" {
     template {
       service_account = var.cloud_scheduler_sa_email
       containers {
-        name =  local.cloud_run_v2_job.container.name
+        name  = local.cloud_run_v2_job.container.name
         image = local.cloud_run_v2_job.container.image
         ports {
           container_port = 8080
