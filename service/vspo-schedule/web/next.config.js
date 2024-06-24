897f04b143dbc4cba349029517a90fd875f138e5
sugar-cat7 <study.king7777@gmail.com>
2024-06-25 00:55:15 +0900

    fix: next-i18next

--- service/vspo-schedule/web/next.config.js
+++ service/vspo-schedule/web/next.config.js
@@ -23,7 +23,7 @@ const withPWA = require("next-pwa")({
   ],
 });
 
-const { i18n } = require('./i18next.config');
+const { i18n } = require('./next-i18next.config');
 
 /** @type {import("next").NextConfig} */
 const nextConfig = {
