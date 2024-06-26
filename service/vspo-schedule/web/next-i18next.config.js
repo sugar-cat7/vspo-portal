39fe17b5e5c911b02f17e204c35b104a171efa7f
sruenwg <155891765+sruenwg@users.noreply.github.com>
2024-06-25 21:45:16 -0700

    fix(web): rename jp locale folder to ja

--- service/vspo-schedule/web/next-i18next.config.js
+++ service/vspo-schedule/web/next-i18next.config.js
@@ -3,9 +3,9 @@ const path = require("path");
 
 module.exports = {
   i18n: {
-    defaultLocale: "ja",
+    defaultLocale: "en",
     locales: ["en", "ja"],
-    localeDetection: true,
   },
   localePath: path.resolve("./public/locales"),
+  reloadOnPrerender: process.env.NODE_ENV === "development",
 };
