73e7119b057aa7482375ee84a0c7663588a9c3c0
sugar-cat7 <study.king7777@gmail.com>
2024-06-28 00:24:16 +0900

    fix

--- service/vspo-schedule/proxy/routes.ts
+++ service/vspo-schedule/proxy/routes.ts
@@ -6,15 +6,15 @@ export const registerOldAPIProxyRoutes = (app: App) => {
         // Send request to Backend API
         const response = await fetch(c.get('requestUrl'), { headers: c.req.raw.headers });
 
-        // Parse response to JSON
-        const data = await response.json();
-
         // Event...
         if (c.req.path.includes('events')) {
             return response
             // const translatedData = await eventProcessor(c, data);
             // return c.json(translatedData);
         }
+
+        // Parse response to JSON
+        const data = await response.json();
 
         // Livestream, freechat, clip.....
         const translatedData = await videoProcessor(c, data);
