6dd68eb0085fa1ea74fe519f5df090fed57ace10
sugar-cat7 <study.king7777@gmail.com>
2024-07-02 06:29:32 +0900

    fix: lang

--- service/vspo-schedule/web/src/pages/_document.tsx
+++ service/vspo-schedule/web/src/pages/_document.tsx
@@ -1,10 +1,13 @@
 import Document, { Html, Head, Main, NextScript } from "next/document";
 import { getInitColorSchemeScript } from "@mui/material/styles";
+import i18nextConfig from "../../next-i18next.config";
 
 class MyDocument extends Document {
   render() {
+    const currentLocale =
+      this.props.__NEXT_DATA__.locale ?? i18nextConfig.i18n.defaultLocale;
     return (
-      <Html lang="ja">
+      <Html lang={currentLocale}>
         <Head>
           {process.env.NODE_ENV === "production" && (
             <script
