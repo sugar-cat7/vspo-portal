8f962b75b6b0bd378901c4452021877e13a6c099
Andrew <155891765+sruenwg@users.noreply.github.com>
2024-07-10 03:50:04 -0700

    Refactor video card components (#472)
    * refactor(web): extract ClipCard from ClipList
    
    * refactor(web): create VideoCard from ClipCard and LivestreamCard

---
+++ service/vspo-schedule/web/src/components/Elements/Card/VideoCard.tsx
@@ -0,0 +1,90 @@
+import Image from "next/image";
+import { Box, Card, CardActionArea } from "@mui/material";
+import { styled } from "@mui/material/styles";
+import { useVideoModalContext } from "@/hooks";
+import { Video } from "@/types/streaming";
+
+type Props = {
+  video: Video;
+  children: React.ReactNode; // CardContent
+  highlight?: {
+    label: string;
+    color: string;
+    bold: boolean;
+  };
+};
+
+const HighlightLabel = styled("div")<{
+  highlightColor: string;
+  bold: boolean;
+}>(({ theme, highlightColor, bold }) => ({
+  minWidth: "78px",
+  padding: "0 12px",
+  color: "white",
+  fontSize: "15px",
+  fontWeight: bold ? "700" : "400",
+  fontFamily: "Roboto, sans-serif",
+  textAlign: "center",
+  lineHeight: "24px",
+  background: highlightColor,
+  borderRadius: "12px",
+  position: "absolute",
+  top: "-12px",
+  right: "6px",
+  zIndex: "3",
+  transformOrigin: "center right",
+  [theme.breakpoints.down("md")]: {
+    transform: "scale(0.875)",
+    right: "5px",
+  },
+  [theme.breakpoints.down("sm")]: {
+    transform: "scale(0.75)",
+    right: "4px",
+  },
+}));
+
+const StyledCard = styled(Card, {
+  shouldForwardProp: (prop) => prop !== "highlightColor",
+})<{ highlightColor?: string }>(({ theme, highlightColor }) => ({
+  display: "flex",
+  flexDirection: "column",
+  height: "100%",
+  width: "100%",
+  border: highlightColor ? `3px solid ${highlightColor}` : "none",
+  backgroundColor: "white",
+  [theme.getColorSchemeSelector("dark")]: {
+    backgroundColor: "#353535",
+  },
+}));
+
+const StyledCardMedia = styled(Box)({
+  paddingTop: "56.25%", // 16:9 aspect ratio
+  objectFit: "contain",
+});
+
+export const VideoCard: React.FC<Props> = ({ video, highlight, children }) => {
+  const { pushVideo } = useVideoModalContext();
+
+  return (
+    <Box sx={{ position: "relative" }}>
+      {highlight && (
+        <HighlightLabel highlightColor={highlight.color} bold={highlight.bold}>
+          {highlight.label}
+        </HighlightLabel>
+      )}
+      <StyledCard highlightColor={highlight?.color}>
+        <CardActionArea onClick={() => pushVideo(video)}>
+          <StyledCardMedia position="relative">
+            <Image
+              src={video.thumbnailUrl}
+              alt={video.title}
+              fill
+              style={{ objectFit: "cover" }}
+            />
+          </StyledCardMedia>
+          {children}
+        </CardActionArea>
+      </StyledCard>
+    </Box>
+  );
+};
