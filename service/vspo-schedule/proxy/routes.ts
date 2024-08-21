af6a005abe6402fd537b504f7bb4061d62b56460
sugar <69241625+sugar-cat7@users.noreply.github.com>
2024-08-21 17:18:49 +0900

    fix: cache handling (#527)

--- service/vspo-schedule/proxy/routes.ts
+++ service/vspo-schedule/proxy/routes.ts
@@ -34,7 +34,9 @@ export const registerOldAPIProxyRoutes = (app: App) => {
 
         // Cache the translated data if it is expired or not found
         if (isExpired || !cache) {
-            c.executionCtx.waitUntil(createCache(kv, requestUrl, JSON.stringify(translatedData), 60));
+            if (translatedData?.length > 0) {
+                c.executionCtx.waitUntil(createCache(kv, requestUrl, JSON.stringify(translatedData), 60));
+            }
         }
 
         return c.json(translatedData);
