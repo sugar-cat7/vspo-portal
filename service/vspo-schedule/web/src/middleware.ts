e538a971ed9d205c61bf875b96842da5906e05d0
sugar <69241625+sugar-cat7@users.noreply.github.com>
2024-08-21 18:04:00 +0900

    chore: debug log (#529)

--- service/vspo-schedule/web/src/middleware.ts
+++ service/vspo-schedule/web/src/middleware.ts
@@ -4,30 +4,35 @@ import { DEFAULT_TIME_ZONE, TIME_ZONE_COOKIE, TIME_ZONE_HEADER } from "./lib/Con
 const publicFileRegex = /\.(.*)$/;
 
 export const middleware = (req: NextRequest) => {
-  const pathname = req.nextUrl.pathname;
-  if (
-    pathname.startsWith("/_next") ||
-    pathname.includes("/api/") ||
-    publicFileRegex.test(pathname)
-  ) {
-    return;
-  }
+  try {
+    const pathname = req.nextUrl.pathname;
+    if (
+      pathname.startsWith("/_next") ||
+      pathname.includes("/api/") ||
+      publicFileRegex.test(pathname)
+    ) {
+      return;
+    }
 
-  // If the client does not have a time zone cookie set (i.e. the request does
-  // not contain a time zone cookie), set the time zone cookie to be the first
-  // valid time zone in:
-  // - x-vercel-ip-timezone header
-  // - default app time zone
-  const cookieTimeZone = getCookieTimeZone(req);
-  if (cookieTimeZone !== undefined) {
+    // If the client does not have a time zone cookie set (i.e. the request does
+    // not contain a time zone cookie), set the time zone cookie to be the first
+    // valid time zone in:
+    // - x-vercel-ip-timezone header
+    // - default app time zone
+    const cookieTimeZone = getCookieTimeZone(req);
+    if (cookieTimeZone !== undefined) {
+      return NextResponse.next();
+    }
+    const headerTimeZone = getHeaderTimeZone(req);
+    const timeZoneToSet = headerTimeZone ?? DEFAULT_TIME_ZONE;
+    setCookie(req, TIME_ZONE_COOKIE, timeZoneToSet);
+    const res = NextResponse.next({ request: req });
+    setCookie(res, TIME_ZONE_COOKIE, timeZoneToSet);
+    return res;
+  } catch (e) {
+    console.error(e);
     return NextResponse.next();
   }
-  const headerTimeZone = getHeaderTimeZone(req);
-  const timeZoneToSet = headerTimeZone ?? DEFAULT_TIME_ZONE;
-  setCookie(req, TIME_ZONE_COOKIE, timeZoneToSet);
-  const res = NextResponse.next({ request: req });
-  setCookie(res, TIME_ZONE_COOKIE, timeZoneToSet);
-  return res;
 };
 
 const getCookieTimeZone = (req: NextRequest) => {
