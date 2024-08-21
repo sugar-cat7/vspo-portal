dc6eb2a4555e08b05d9d11624da269d49eea873c
sugar-cat7 <study.king7777@gmail.com>
2024-08-21 19:28:05 +0900

    fix

--- service/vspo-schedule/web/src/pages/schedule/[status].tsx
+++ service/vspo-schedule/web/src/pages/schedule/[status].tsx
@@ -146,7 +146,6 @@ export const getServerSideProps: GetServerSideProps<
     const timeZone = req.cookies[TIME_ZONE_COOKIE] ?? DEFAULT_TIME_ZONE;
     const isDateStatus = isValidDate(params.status);
 
-    console.info("[DEBUG1]", params.status, timeZone);
     // Logic 1: Fetch uniqueLivestreams
     const fetchLivestreamsData = async () => {
       const { oneWeekAgo, oneWeekLater } = getOneWeekRange();
@@ -322,8 +321,6 @@ export const getServerSideProps: GetServerSideProps<
     const description = `${t("description")}\n${livestreamDescription}`;
 
     const filteredLivestreams = uniqueLivestreams;
-    console.info("[DEBUG2]", JSON.stringify(filteredLivestreams));
-    console.info("[DEBUG3]", JSON.stringify(events));
 
     const livestreamsByDate = groupBy(filteredLivestreams, (livestream) => {
       try {
