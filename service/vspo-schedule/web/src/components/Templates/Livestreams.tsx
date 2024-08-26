b8665202e648c5f213a6ea6dcb5e20d067e2d69f
sruenwg <155891765+sruenwg@users.noreply.github.com>
2024-08-26 15:01:22 -0700

    refactor(web): reduce duplication in conditionally wrapped elements

--- service/vspo-schedule/web/src/components/Templates/Livestreams.tsx
+++ service/vspo-schedule/web/src/components/Templates/Livestreams.tsx
@@ -153,6 +153,11 @@ export const LivestreamCards: React.FC<Props> = ({
                     const eventMembers = getRelevantMembers(
                       event.contentSummary,
                     );
+                    const eventTitle = (
+                      <Typography sx={{ fontSize: "16px" }}>
+                        ・{event.title}
+                      </Typography>
+                    );
                     return (
                       <Box
                         display="flex"
@@ -164,22 +169,10 @@ export const LivestreamCards: React.FC<Props> = ({
                         key={eventIndex}
                       >
                         {event.isNotLink ? (
-                          <Typography
-                            sx={{
-                              fontSize: "16px",
-                            }}
-                          >
-                            ・{event.title}
-                          </Typography>
+                          eventTitle
                         ) : (
                           <Link href={`/events/details/${event.newsId}`}>
-                            <Typography
-                              sx={{
-                                fontSize: "16px",
-                              }}
-                            >
-                              ・{event.title}
-                            </Typography>
+                            {eventTitle}
                           </Link>
                         )}
                         {eventMembers.length > 0 && (
