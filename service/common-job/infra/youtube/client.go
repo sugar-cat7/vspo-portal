aebc7f6fc85ce2c203192b6f6e3a48ba770f8463
sugar-cat7 <study.king7777@gmail.com>
2024-10-16 02:28:44 +0900

    fix: client

--- service/common-job/infra/youtube/client.go
+++ service/common-job/infra/youtube/client.go
@@ -3,9 +3,7 @@ package youtube
 import (
 	"context"
 	"log"
-	"time"
 
-	"github.com/sugar-cat7/vspo-portal/service/common-job/pkg/http"
 	"google.golang.org/api/option"
 	"google.golang.org/api/youtube/v3"
 )
@@ -13,7 +11,8 @@ import (
 // NewClient returns a new YouTube client.
 func NewClient(apiKey string) *youtube.Service {
 	ctx := context.Background()
-	client, err := youtube.NewService(ctx, option.WithAPIKey(apiKey), option.WithHTTPClient(http.NewHttpClient(10*time.Second)))
+	// client, err := youtube.NewService(ctx, option.WithAPIKey(apiKey), option.WithHTTPClient(http.NewHttpClient(10*time.Second)))
+	client, err := youtube.NewService(ctx, option.WithAPIKey(apiKey))
 	if err != nil {
 		log.Fatalf("Error creating new YouTube client: %v", err)
 	}
